bbed3bfd918dd25ece35c6cb324871f8c87fa84d
Vinicius de Santana <vinicius.vts@gmail.com>
2020-07-09 23:38:09 -0300

    create categorias and context

---
+++ WebMVC/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string descricao { get; set; }
+    }
+}
---
+++ WebMVC/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=webmvc;user=root;password=root");
+        }
+    }
+}
